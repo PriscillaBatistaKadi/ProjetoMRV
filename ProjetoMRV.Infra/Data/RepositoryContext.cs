8b96986c9a3569be577d43dde3776748aa97419d
PriscillaBatistaKadi <prilimakadi@gmail.com>
2024-10-27 13:27:04 -0300

    Add files via upload

---
+++ ProjetoMRV.Infra/Data/RepositoryContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoMRV.Domain.Entities;
+
+namespace ProjetoMRV.Infra.Data
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions<RepositoryContext> options) : base(options) { }
+
+        public DbSet<SpaEntity> Spa { get; set; }
+    }
+}
---
+++ ProjetoMRV.Infra/Repository/SpaRepository.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoMRV.Domain.Command;
+using ProjetoMRV.Domain.CommandResult;
+using ProjetoMRV.Domain.Entities;
+using ProjetoMRV.Domain.Enums;
+using ProjetoMRV.Domain.Repository.Interfaces;
+using ProjetoMRV.Infra.Data;
+using System;
+using System.Data;
+
+namespace ProjetoMRV.Infra.Repository
+{
+    public class SpaRepository : ISpaRepository
+    {
+        private readonly RepositoryContext _context;
+
+        public SpaRepository(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SpaEntity>> GetSpaRepository()
+        {
+            try
+            {
+                var result = await _context.Spa.Where(x => x.Status == (int)EnumStatus.Invited).ToListAsync();
+
+                return result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<List<SpaEntity>> GetAcceptedsRepository()
+        {
+            try
+            {
+                var result = await _context.Spa.Where(x => x.Status == (int)EnumStatus.Accepted).ToListAsync();
+
+                return result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<bool> AcceptSpaRepository(SpaCommand command)
+        {
+            try
+            {
+                var spaToUpdate = await _context.Spa.FindAsync(command.Id);
+
+                if (spaToUpdate != null)
+                {
+                    spaToUpdate.Status = (int)EnumStatus.Accepted;
+
+                    if (spaToUpdate.Price > 500)
+                    {
+                        spaToUpdate.Price *= 0.9;
+                    }
+
+                    await _context.SaveChangesAsync();
+                }
+                else
+                {
+                    return false;
+                }
+
+                return true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<bool> DeclineSpaRepository(SpaCommand command)
+        {
+            try
+            {
+                var spaToUpdate = await _context.Spa.FindAsync(command.Id);
+
+                if (spaToUpdate != null)
+                {
+                    spaToUpdate.Status = (int)EnumStatus.Decline;
+
+                    await _context.SaveChangesAsync();
+                }
+                else
+                {
+                    return false;
+                }
+
+                return true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+    }
+}
